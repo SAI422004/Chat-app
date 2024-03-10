5c3216b9a2d9e27615c5c86ac79300c0a86ad321
SAI422004 <sohamnikam84@gmail.com>
2024-03-10 13:19:58 +0530

    Deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv"; //package imports
 import express from "express"; //Package imports
 import cookieParser from 'cookie-parser';
@@ -14,6 +15,8 @@ import mongoose from "mongoose";//file imports
 const app = express();  //variables
 const PORT = process.env.PORT || 5000; //variables
 
+const __dirname = path.resolve();
+
 mongoose.connect("mongodb://localhost:27017/")
 .then(()=>console.log("connected to mongodb"))
 .catch((err)=>console.log(err))
@@ -29,6 +32,12 @@ app.use("/api/users", userRoutes);
 // app.get('/', (req, res) => {
 //     res.send("Hello World!!");
 // });
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.listen(PORT, () => {
     //connectToMongoDB();
